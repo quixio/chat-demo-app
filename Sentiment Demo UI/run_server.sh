b170366768a2b0c98a52fa361e17700f9ccca0a6
Chris Gilchrist <chris@quix.ai>
2023-08-15 16:25:42 +0100

    Added new topic drafts_sentiment.

--- Sentiment Demo UI/run_server.sh
+++ Sentiment Demo UI/run_server.sh
@@ -5,4 +5,5 @@ echo "${Quix__Portal__Api}" > /usr/share/nginx/html/portal_api
 echo "${sentiment}" > /usr/share/nginx/html/sentiment_topic
 echo "${messages}" > /usr/share/nginx/html/messages_topic
 echo "${drafts}" > /usr/share/nginx/html/drafts_topic
+echo "${drafts_sentiment}" > /usr/share/nginx/html/drafts_sentiment_topic
 nginx -g "daemon off;"
