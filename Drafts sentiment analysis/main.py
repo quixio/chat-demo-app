deab0c3618ebb36892f41af6c094e7ce96a6a1ad
Haris Botic <haris+demo@quix.io>
2023-08-15 10:49:25 +0000

    Edited 'main.py'

--- Drafts sentiment analysis/main.py
+++ Drafts sentiment analysis/main.py
@@ -24,7 +24,7 @@ def read_stream(consumer_stream: qx.StreamConsumer):
     producer_stream.properties.parents.append(consumer_stream.stream_id)
 
     # Create a stream state
-    state = consumer_stream.get_dict_state("drafts", lambda key: None)
+    state = consumer_stream.get_dict_state("drafts", lambda missing_key: None)
 
     # handle the data in a function to simplify the example
     quix_function = QuixFunction(consumer_stream, producer_stream, classifier, state)
