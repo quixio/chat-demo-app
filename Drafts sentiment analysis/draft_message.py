4f774964d090971ae43f39fcbc88e4a4a6839a6b
Haris Botic <haris+demo@quix.io>
2023-08-15 09:17:05 +0000

    Edited 'draft_message.py'

--- Drafts sentiment analysis/draft_message.py
+++ Drafts sentiment analysis/draft_message.py
@@ -7,11 +7,6 @@ class DraftMessage:
 
     DIVIDER = "|"
 
-    @property
-    def _started_writing_ms(self) -> int:
-        """Converts the nanoseconds timestamp to milliseconds."""
-        return self.created_at_ns // 1_000_000  # Convert ns to ms
-        
     def get_typing_duration_ms(self, end_time_ns: int) -> int:
         """Calculates the typing duration in milliseconds from a given end time in nanoseconds."""
         duration_ns = end_time_ns - self.created_at_ns
@@ -34,4 +29,4 @@ class DraftMessage:
 
     def __str__(self) -> str:
         """Returns the string representation of the DraftMessage."""
-        return f"{self.draft_id}{DraftMessage.DIVIDER}{self._started_writing_ms}"
+        return f"{self.draft_id}{DraftMessage.DIVIDER}{self.created_at_ns}"
