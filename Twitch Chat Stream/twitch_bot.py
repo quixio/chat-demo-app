ec88706bdb1345df21718c1157a1108133f31575
Haris Botic <haris+demo@quix.io>
2023-08-17 17:40:25 +0000

    Edited 'twitch_bot.py'

--- Twitch Chat Stream/twitch_bot.py
+++ Twitch Chat Stream/twitch_bot.py
@@ -5,18 +5,20 @@ from twitchio.ext import commands
 
 class Bot(commands.Bot):
 
-    def __init__(self, token: str, channels_to_join: List[str], on_message_handler):
+    def __init__(self, token: str, on_ready_handler, on_message_handler):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
         self.on_message_handler = on_message_handler 
-        super().__init__(token=token, prefix='!', initial_channels=channels_to_join)
+        self.on_ready_handler = on_ready_handler
+        super().__init__(token=token, prefix='!')
 
     async def event_ready(self):
         # Notify us when everything is ready!
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
+        self.on_ready_handler()
 
     async def event_message(self, message: twitchio.Message):
         # Messages with echo set to True are messages sent by the bot...
