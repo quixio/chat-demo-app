8d8dae43b65effcea43680af195822bce062e116
Haris Botic <haris+demo@quix.io>
2023-08-17 20:03:34 +0000

    Edited 'twitch_api.py'

--- Twitch Chat Stream/twitch_api.py
+++ Twitch Chat Stream/twitch_api.py
@@ -33,7 +33,7 @@ def _get_top_streams(oauth_token: str, limit: int):
     response = requests.get(f"{BASE_URL}streams", headers=headers, params=params)
     return response.json().get('data', [])
 
-def _get_live_streams_from_users(oauth_token: str, user_logins: List[str]):
+def _get_live_streams_by_users(oauth_token: str, user_logins: List[str]):
     headers = {
         **HEADERS,
         'Authorization': f"Bearer {oauth_token}"
@@ -50,3 +50,9 @@ def get_top_streams(limit: int = 50):
     top_streams = _get_top_streams(oauth_token, limit)
 
     return [stream["user_login"] for stream in top_streams]
+
+def get_live_streams_by_users(user_logins: List[str]):
+    oauth_token = get_oauth_token(CLIENT_ID, CLIENT_SECRET)
+    live_streams = _get_top_streams(oauth_token, user_logins)
+
+    return [stream["user_login"] for stream in live_streams]
