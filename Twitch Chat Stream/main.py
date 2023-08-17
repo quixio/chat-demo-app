19472a4b1dafd9f1032db0cae865bcfa7b3eb5c6
Haris Botic <haris+demo@quix.io>
2023-08-17 11:48:56 +0000

    Edited 'main.py'

--- Twitch Chat Stream/main.py
+++ Twitch Chat Stream/main.py
@@ -4,7 +4,7 @@ from twitch_bot import Bot
 twitch_token = os.environ["TwitchToken"]
 channels_to_join = ["xQc", "ElMariana", "PaymoneyWubby", "rivers_gg", "Rubius", "eliasn97", "sneakylol", "Caedrel", "Becca", "Fextralife"]
 
-print(token)
+print(twitch_token)
 
 bot = Bot(token=twitch_token, channels_to_join=channels_to_join)
 bot.run()
