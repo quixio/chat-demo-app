ef4d582b37eb1fd82b63bfd30679f22d838b91bb
Haris Botic <haris+demo@quix.io>
2023-08-17 11:30:13 +0000

    Edited 'main.py'

--- Twitch Chat Stream/main.py
+++ Twitch Chat Stream/main.py
@@ -1,73 +1,32 @@
-import quixstreams as qx
-import time
-import os
+import asyncio
+import websockets
 import json
-import websocket
 
-twitch_channel_name = os.environ["TwitchChannel"]
-twitch_nickname = os.environ["TwitchNickname"]
-twitch_oauth = os.environ["TwitchToken"]
+NICK = 'your_twitch_username'
+TOKEN = 'oauth:your_twitch_oauth_token'  # Use https://twitchapps.com/tmi/ to get your OAuth token
+CHANNELS = ['channel1', 'channel2']  # List of channels you want to join
 
-twitch_channels_to_join = ["xQc", "ElMariana", "PaymoneyWubby", "rivers_gg", "Rubius", "eliasn97", "sneakylol", "Caedrel", "Becca", "Fextralife"]
+async def join_channel(channel_name):
+    uri = f"wss://irc-ws.chat.twitch.tv:443"
+    
+    async with websockets.connect(uri) as ws:
+        await ws.send(f"PASS {TOKEN}")
+        await ws.send(f"NICK {NICK}")
+        await ws.send(f"JOIN #{channel_name}")
 
-# Quix Platform injects credentials automatically to the client.
-# Alternatively, you can always pass an SDK token manually as an argument.
-client = qx.QuixStreamingClient()
+        while True:
+            message = await ws.recv()
+            if "PRIVMSG" in message:
+                # Extract username and message from the raw message string
+                username = message.split('!', 1)[0][1:]
+                chat_message = message.split('PRIVMSG', 1)[1].split(':', 1)[1]
+                print(f"[{channel_name}] {username}: {chat_message}")
+            elif "PING" in message:
+                await ws.send("PONG :tmi.twitch.tv")
 
-print(f"Opening producer topic: {os.environ['Topic']}")
-# The producer topic is where the data will be published to
-# It's the output from this demo data source code.
-topic_producer = client.get_topic_producer(os.environ["Topic"])
+async def main():
+    tasks = [join_channel(channel) for channel in CHANNELS]
+    await asyncio.gather(*tasks)
 
-
-def publish_chat_message(user: str, message: str, channel: str, role: str = "Customer"):
-    # create a Timeseries Data
-    timeseries_data = qx.TimeseriesData()
-
-    timeseries_data \
-        .add_timestamp_nanoseconds(time.time_ns()) \
-        .add_value("chat-message", message) \
-        .add_tags({"room": channel, "name": user, "role": role})
-
-    # publish the data to the Quix stream created earlier
-    stream_producer = topic_producer.get_or_create_stream(channel)
-    stream_producer.timeseries.publish(timeseries_data)
-
-
-def on_message(ws, message):
-    data = json.loads(message)
-    if data['type'] == 'MESSAGE':
-        channel = data['data']['channel_name']
-        user = data['data']['from_broadcaster_user']['display_name']
-        msg = data['data']['text']
-        print(f"[{channel}] {user}: {msg}")
-
-        publish_chat_message(user=user, message=msg, channel=channel)
-
-def on_open(ws):
-    for channel in twitch_channels_to_join:
-        ws.send(json.dumps({
-            "type": "LISTEN",
-            "nonce": "noncename",  # you can replace this with any unique string
-            "data": {
-                "topics": [f"channel-points-channel-v1.{channel}", f"chat_moderator_actions.{channel}", f"whispers.{channel}"],
-                "auth_token": twitch_oauth  # Replace with your OAuth token
-            }
-        }))
-
-    print("Connection opened!")
-
-def on_error(ws, error):
-    print(error)
-
-def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
-
-websocket.enableTrace(True)
-ws = websocket.WebSocketApp("wss://irc-ws.chat.twitch.tv/",
-                            on_message=on_message,
-                            on_error=on_error,
-                            on_close=on_close,
-                            on_open=on_open)
-
-ws.run_forever()
+if __name__ == '__main__':
+    asyncio.run(main())
