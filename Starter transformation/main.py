7d61235b67b6d23832eff1b494b57592d67f64c7
Quix Demo <demo@quix.ai>
2023-08-17 10:15:14 +0000

    Edited 'main.py'

--- Starter transformation/main.py
+++ Starter transformation/main.py
@@ -5,32 +5,23 @@ import pandas as pd
 
 client = qx.QuixStreamingClient()
 
-topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "empty-transformation")
+topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "counter", auto_offset_reset=qx.AutoOffsetReset.Earliest)
 topic_producer = client.get_topic_producer(os.environ["output"])
 
 
-def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+def on_data_released(stream_consumer: qx.StreamConsumer, data: qx.TimeseriesData):
 
-    # Transform data frame here in this method. You can filter data or add new features.
-    # Pass modified data frame to output stream using stream producer.
-    # Set the output stream id to the same as the input stream or change it,
-    # if you grouped or merged data with different key.
-    stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
-    stream_producer.timeseries.buffer.publish(df)
+    stream_producer = topic_producer.get_or_create_stream(stream_id = "count")
+    stream_producer.timeseries.buffer.add_timestamp_nanoseconds(data.add_timestamp_nanoseconds) \
+        .add_value("count", len(data.timestamps)) \
+        .publish()
 
-
-# Handle event data from samples that emit event data
-def on_event_data_received_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
-    print(data)
-    # handle your event data here
 
 
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
-    # subscribe to new DataFrames being received
-    # if you aren't familiar with DataFrames there are other callbacks available
-    # refer to the docs here: https://docs.quix.io/sdk/subscribe.html
-    stream_consumer.events.on_data_received = on_event_data_received_handler # register the event data callback
-    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
+    buffer = stream_consumer.timeseries.create_buffer()
+    buffer.time_span_in_milliseconds = 1000
+    buffer.on_data_released = on_data_released
 
 
 # subscribe to new streams being received
