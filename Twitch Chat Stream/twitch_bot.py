e8668e19fb149f7eb207bbad29af1dd0e5712224
Haris Botic <haris+demo@quix.io>
2023-08-17 18:58:38 +0000

    Edited 'twitch_bot.py'

--- Twitch Chat Stream/twitch_bot.py
+++ Twitch Chat Stream/twitch_bot.py
@@ -43,3 +43,13 @@ class Bot(commands.Bot):
         await self.part_channels(offline_channel_names)
 
         print(f'Parted from channels: {offline_channel_names}')
+
+    async def get_unjoined_top_streams(self, count: int):
+        connected_channel_names = [channel.name for channel in self.connected_channels]
+        live_channels = await self.fetch_streams(user_logins=connected_channel_names, type='live')
+        live_channel_names = [stream.user.name for stream in live_channels]
+        offline_channel_names = list(set(connected_channel_names) - set(live_channel_names))
+
+        await self.part_channels(offline_channel_names)
+
+        print(f'Parted from channels: {offline_channel_names}')
