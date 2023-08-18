059395025290e0a7da43b59a3cefb9cb627d8a9a
Haris Botic <haris+demo@quix.io>
2023-08-18 12:50:04 +0000

    Edited 'main.py'

--- Starter transformation/main.py
+++ Starter transformation/main.py
@@ -1,6 +1,6 @@
 import quixstreams as qx
 import os
-
+from tqdm import tqdm
 
 client = qx.QuixStreamingClient()
 
@@ -13,18 +13,21 @@ def on_data_released(stream_consumer: qx.StreamConsumer, data: qx.TimeseriesData
     stream_producer.timeseries.buffer.add_timestamp_nanoseconds(data.timestamps[0].timestamp_nanoseconds) \
         .add_value("count", len(data.timestamps)) \
         .publish()
+    bar.update()
     print(str(data.timestamps[0].timestamp_milliseconds) + " - " + str(data.timestamps[-1].timestamp_milliseconds) + " count: " + str(len(data.timestamps)))
 
 
 
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
     buffer = stream_consumer.timeseries.create_buffer()
-    buffer.time_span_in_milliseconds = 1000
     buffer.on_data_released = on_data_released
 
 
 # subscribe to new streams being received
 topic_consumer.on_stream_received = on_stream_received_handler
+
+
+bar = tqdm(desc="Receiving messages")
 
 # Handle termination signals and provide a graceful exit
 qx.App.run()
