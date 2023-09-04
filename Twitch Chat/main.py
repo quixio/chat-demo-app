eb0f9abd66b4ee5722ac734a53243b6728d774d5
Haris Botic <haris+demo@quix.io>
2023-09-04 08:13:59 +0000

    Edited 'main.py'

--- Twitch Chat/main.py
+++ Twitch Chat/main.py
@@ -10,12 +10,15 @@ client = qx.QuixStreamingClient()
 
 topic_producer = client.get_topic_producer(os.environ["output"])
 
-def publish_chat_message(user: str, message: str, channel: str, role: str = "Customer"):
+def publish_chat_message(user: str, message: str, channel: str, timestamp, role: str = "Customer"):
     timeseries_data = qx.TimeseriesData()
     timeseries_data \
         .add_timestamp_nanoseconds(time.time_ns()) \
         .add_value("chat-message", message) \
         .add_tags({"room": "channel", "name": user, "role": role})
+
+    print(timestamp)
+    print(type(timestamp))
 
     stream_producer = topic_producer.get_or_create_stream(channel)
     stream_producer.timeseries.publish(timeseries_data)
