2f1c22f28a96ea87c51af10e9cb9781308e1d06d
Chris Gilchrist <gilly@sgnet.co.uk>
2023-09-19 16:18:34 +0100

    Updated chart speed to even slower.

--- Sentiment Demo UI/src/app/components/sentiment-chart/sentiment-chart.component.ts
+++ Sentiment Demo UI/src/app/components/sentiment-chart/sentiment-chart.component.ts
@@ -41,7 +41,7 @@ export class SentimentChartComponent implements OnInit, OnDestroy {
       x: {
         type: 'realtime',
         realtime: {
-          duration: 40000,
+          duration: 200000,
           refresh: 1000,
           delay: 200,
           onRefresh: (chart: Chart) => {
