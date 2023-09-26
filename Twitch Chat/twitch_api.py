1fd22aa192c925946cdfc98b9e692227fa01676e
Haris Botic <haris+demo@quix.io>
2023-09-26 09:08:47 +0000

    Edited 'twitch_api.py'

--- Twitch Chat/twitch_api.py
+++ Twitch Chat/twitch_api.py
@@ -63,26 +63,38 @@ def get_live_streams_by_users(user_logins: List[str]) -> List["TwitchStream"]:
 
 @dataclass
 class TwitchStream:
-    user_login: str
+    id: str
     user_id: str
+    user_login: str
+    user_name: str
     game_id: str
     game_name: str
+    type: str
     title: str
     viewer_count: int
     started_at: str
     language: str
     thumbnail_url: str
+    tag_ids: List[str]
+    tags: List[str]
+    is_mature: bool
 
     @classmethod
     def from_dict(cls, data):
         return cls(
-            user_login=data["user_login"],
-            user_id=data["user_id"],
-            game_id=data["game_id"],
-            game_name=data["game_name"],
-            title=data["title"],
-            viewer_count=data["viewer_count"],
-            started_at=data["started_at"],
-            language=data["language"],
-            thumbnail_url=data["thumbnail_url"]
+            id=data['id'],
+            user_id=data['user_id'],
+            user_login=data['user_login'],
+            user_name=data['user_name'],
+            game_id=data['game_id'],
+            game_name=data['game_name'],
+            type=data['type'],
+            title=data['title'],
+            viewer_count=data['viewer_count'],
+            started_at=data['started_at'],
+            language=data['language'],
+            thumbnail_url=data['thumbnail_url'],
+            tag_ids=data['tag_ids'],
+            tags=data['tags'],
+            is_mature=data['is_mature']
         )
