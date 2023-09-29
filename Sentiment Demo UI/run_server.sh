db6eb601c6a31313cea536055c13fd4453187d4a
Chris Gilchrist <gilly@sgnet.co.uk>
2023-09-29 15:20:33 +0100

    Added new topic for twitch messages. Added pipe to filter the messages when its a twitch stream so that it only shows the ones which have a sentiment.

--- Sentiment Demo UI/run_server.sh
+++ Sentiment Demo UI/run_server.sh
@@ -4,6 +4,7 @@ echo "${Quix__Sdk__Token}" > /usr/share/nginx/html/sdk_token
 echo "${Quix__Portal__Api}" > /usr/share/nginx/html/portal_api
 echo "${sentiment}" > /usr/share/nginx/html/sentiment_topic
 echo "${messages}" > /usr/share/nginx/html/messages_topic
+echo "${twitch_messages}" > /usr/share/nginx/html/twitch_messages_topic
 echo "${drafts}" > /usr/share/nginx/html/drafts_topic
 echo "${drafts_sentiment}" > /usr/share/nginx/html/drafts_sentiment_topic
 nginx -g "daemon off;"
