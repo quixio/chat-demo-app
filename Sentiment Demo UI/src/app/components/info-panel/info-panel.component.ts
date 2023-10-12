881f9cf0aa75efa8b3efdd7c67143991acd37d62
Steve <100689438+SteveRosam@users.noreply.github.com>
2023-10-12 16:35:37 +0100

    updates to info panel links

--- Sentiment Demo UI/src/app/components/info-panel/info-panel.component.ts
+++ Sentiment Demo UI/src/app/components/info-panel/info-panel.component.ts
@@ -14,16 +14,21 @@ export class InfoPanelComponent implements OnInit, OnDestroy {
   ungatedToken: string;
   deploymentId: string;
 
+  // deployment id's for the PROD deployments. used by links in the info panel.
+  public sentimentAnalysisDeploymentId: string = "64aa05e9-b8d7-41d0-89d9-8c7996bd3a15"; // links from the info text in the left hand panel use this to link you to the project in the platform. Easier to leave it blank.
+  public twitchSentimentAnalysisDeploymentId: string = "bcab2636-5092-4ea7-920f-f921c2cbae0f"; // links from the info text in the left hand panel use this to link you to the project in the platform. Easier to leave it blank.
+  
   private unsubscribe$ = new Subject<void>();
 
   constructor(private quixService: QuixService, private roomService: RoomService) { 
+    this.deploymentId = this.sentimentAnalysisDeploymentId; // default to this
     this.workspaceId = this.quixService.workspaceId;
     this.ungatedToken = this.quixService.ungatedToken;
    
     // Listen for the room change to determine whether its a twitch stream or not, 
     // if so then we point to the twitch sentiment deployment.
     this.roomService.roomChanged$.pipe(takeUntil(this.unsubscribe$)).subscribe(({ isTwitch }) => {
-      this.deploymentId = isTwitch ? this.quixService.twitchSentimentAnalysisDeploymentId : this.quixService.sentimentAnalysisDeploymentId
+      this.deploymentId = isTwitch ? this.twitchSentimentAnalysisDeploymentId : this.sentimentAnalysisDeploymentId
     });
   }
 
