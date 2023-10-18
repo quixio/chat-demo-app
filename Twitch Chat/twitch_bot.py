c265b2178a966cc38b08f5242f77df94c25d3ed1
Haris Botic <haris+demo@quix.io>
2023-10-18 17:12:41 +0000

    Edited 'twitch_bot.py'

--- Twitch Chat/twitch_bot.py
+++ Twitch Chat/twitch_bot.py
@@ -54,6 +54,9 @@ class Bot(commands.Bot):
             
             await asyncio.sleep(15)  # Wait for 15 seconds between batches
 
+        print(f'Parting from {len(offline_channel_names)} channels: {offline_channel_names}')
+        await self.part_channels(offline_channel_names)
+
         return top_live_channel_dict.values()
 
 
